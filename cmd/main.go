e5a7ed927e35198a7a378c52c9d43d99839690dc
Антонина Приселкова <chigineva89@gmail.com>
2025-05-09 19:52:22 +0300

    fist iteration

--- cmd/main.go
+++ cmd/main.go
@@ -5,14 +5,11 @@ import (
 	"log"
 	"os"
 
-	//"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
-	// "github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
 )
 
 func main() {
-	// service.Conver()
-
+	// Создаем логгер
 	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// Создаем сервер
